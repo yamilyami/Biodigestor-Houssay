1b37cbbd45b80cf5a333242a1a2c6e8971ba45c0
mroxanam <mroxanam20@gmail.com>
2024-09-16 20:43:54 -0300

    txt

--- appBio/src/app/app.component.ts
+++ appBio/src/app/app.component.ts
@@ -3,12 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NadvarComponent } from './shared/nadvar/nadvar.component';
 import { CarruselprinComponent } from './shared/carruselprin/carruselprin.component';
 import { FormulaRegistroComponent } from './registro/formula-registro/formula-registro.component';
-
+import { QuienessomosComponent } from './QuienesSomos/quienessomos/quienessomos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NadvarComponent, CarruselprinComponent, FormulaRegistroComponent],
+  imports: [RouterOutlet, NadvarComponent, CarruselprinComponent, FormulaRegistroComponent,QuienessomosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
