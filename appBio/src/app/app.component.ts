2a5b8cd78a7c2ac6953704d4391e5a59fd81253c
Paz-Santangelo <paz_santangelo@hotmail.com>
2024-09-17 16:16:37 -0300

    Agregando seccion breve de quienes somos a Home y componente footer

--- appBio/src/app/app.component.ts
+++ appBio/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NadvarComponent } from './shared/nadvar/nadvar.component';
 import { HomePageComponent } from './shared/home-page/home-page.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NadvarComponent, HomePageComponent],
+  imports: [RouterOutlet, NadvarComponent, HomePageComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
