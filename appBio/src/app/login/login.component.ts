68f2c8910126265bcde4559fb69ab6a17ec3f3d3
Alejandro-Santangelo <artesaniaslarustica@gmail.com>
2024-09-19 21:01:05 -0300

    subiendo carpeta login

---
+++ appBio/src/app/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],  // Aquí puedes agregar módulos necesarios si los hay
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']  // Asegúrate de usar 'styleUrls' en lugar de 'styleUrl'
+})
+export class LoginComponent {}
+
