5dc0d4bb67f2a5722e566190c6b4d4215209bb31
Chiara <chiaraseco7@gmail.com>
2024-09-26 15:28:16 -0300

    Subo funcion agregada recuperar contraseña

--- appBio/src/app/app.component.ts
+++ appBio/src/app/app.component.ts
@@ -3,11 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { NadvarComponent } from './shared/nadvar/nadvar.component';
 import { HomePageComponent } from './shared/home-page/home-page.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { LoginComponent } from "./login/login.component";
+import { RestablecerContrasenaComponent } from './restablecer-contrasena/restablecer-contrasena.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NadvarComponent, HomePageComponent, FooterComponent],
+  imports: [RouterOutlet, NadvarComponent, HomePageComponent, FooterComponent, LoginComponent, RestablecerContrasenaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- appBio/src/app/restablecer-contrasena/restablecer-contrasena.component.ts
+++ appBio/src/app/restablecer-contrasena/restablecer-contrasena.component.ts
@@ -1,12 +1,48 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { VentanaEmergenteService } from '../servicios/ventana-emergente.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-restablecer-contrasena',
   standalone: true,
-  imports: [],
+
+  imports: [CommonModule, FormsModule],
   templateUrl: './restablecer-contrasena.component.html',
-  styleUrl: './restablecer-contrasena.component.css'
+  styleUrls: ['./restablecer-contrasena.component.css']
 })
 export class RestablecerContrasenaComponent {
+  email: string = '';
+  password: string = '';
+  resetEmail: string = '';
+  errorMessage: string = '';
 
+  public isModalOpen: boolean = false;
+  constructor(public ventanaEmergenteService: VentanaEmergenteService) {
+    // Suscribirse al estado del modal
+    this.ventanaEmergenteService.modalState$.subscribe(isOpen => {
+      this.isModalOpen = isOpen;
+    });
+  }
+
+  // Simulación de verificación de correo
+  checkEmail() {
+    if (this.resetEmail === 'test@example.com') {
+      alert('Correo válido. Se ha enviado un enlace de restablecimiento');
+      this.resetEmail = '';
+      this.ventanaEmergenteService.cerrarVentanaEmergente();  // Cerrar el modal usando el servicio
+    } else {
+      this.errorMessage = 'Correo no válido';
+    }
+  }
+
+  // Abrir la ventana modal usando el servicio
+  openPasswordResetModal() {
+    this.ventanaEmergenteService.abrirVentanaEmergente();
+  }
+
+  // Cerrar la ventana modal usando el servicio
+  closePasswordResetModal() {
+    this.ventanaEmergenteService.cerrarVentanaEmergente();
+  }
 }
