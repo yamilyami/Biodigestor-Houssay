7ea3b41380d4180dda1f649bb6c80b17246fe00d
Paz-Santangelo <paz_santangelo@hotmail.com>
2024-09-26 13:43:51 -0300

    Agregando ruteo a componentes de home, quienesSomos y registro

--- appBio/src/app/app.component.ts
+++ appBio/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NadvarComponent } from './shared/nadvar/nadvar.component';
 import { HomePageComponent } from './shared/home-page/home-page.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { QuienessomosComponent } from './QuienesSomos/quienessomos/quienessomos.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NadvarComponent, HomePageComponent, FooterComponent],
+  imports: [RouterOutlet, NadvarComponent, HomePageComponent, FooterComponent, QuienessomosComponent, RegistroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- appBio/src/app/app.routes.ts
+++ appBio/src/app/app.routes.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-import { FormulaRegistroComponent } from './registro/formula-registro/formula-registro.component';
 import { QuienessomosComponent } from './QuienesSomos/quienessomos/quienessomos.component';
-import path from 'node:path';
-/* import { FormulaRegistroComponent } from './registro/formula-registro/formula-registro.component'; */
+import { HomePageComponent } from './shared/home-page/home-page.component';
+import { RegistroComponent } from './registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: HomePageComponent },
+  { path: "quienes-somos", component: QuienessomosComponent },
+  { path: "registrarse", component: RegistroComponent }
+];
