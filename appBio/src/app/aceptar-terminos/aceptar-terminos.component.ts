c5121305fa9784db1e65b3ae364075f50c050f52
Alejandro-Santangelo <artesaniaslarustica@gmail.com>
2024-09-26 16:49:38 -0300

    subiendo modificacion Aceptar-terminos

--- appBio/src/app/aceptar-terminos/aceptar-terminos.component.ts
+++ appBio/src/app/aceptar-terminos/aceptar-terminos.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Asegúrate de importar FormsModule
 
 @Component({
   selector: 'app-aceptar-terminos',
   standalone: true,
-  imports: [],
+  imports: [FormsModule], // Agrega FormsModule aquí
   templateUrl: './aceptar-terminos.component.html',
-  styleUrl: './aceptar-terminos.component.css'
+  styleUrls: ['./aceptar-terminos.component.css'] // Corrige 'styleUrl' a 'styleUrls'
 })
 export class AceptarTerminosComponent {
-
+  // Método que se ejecuta al enviar el formulario
+  onSubmit() {
+    // Lógica a ejecutar al enviar el formulario
+    console.log('Formulario enviado');
+  }
 }
+
