63a624ca835e5c79dbc7d7aaeceb6af1e712025c
Paz-Santangelo <paz_santangelo@hotmail.com>
2024-09-29 22:22:54 -0300

    Se modifica el diseño del navbar para una mejor accesibilidad y visualizacion de los links en todas las rutas

--- appBio/src/app/shared/nadvar/nadvar.component.ts
+++ appBio/src/app/shared/nadvar/nadvar.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nadvar',
   standalone: true,
-  imports: [RouterLink ],
+  imports: [RouterLink],
   templateUrl: './nadvar.component.html',
   styleUrl: './nadvar.component.css'
 })
-export class NadvarComponent {
-
-}
+export class NadvarComponent {}
